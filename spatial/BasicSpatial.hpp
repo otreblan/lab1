a92252ef7faf8694167fac40944010f95063363d
otreblan <otreblain@gmail.com>
2021-09-10 07:18:48 -0500

    Implementación

--- spatial/BasicSpatial.hpp
+++ spatial/BasicSpatial.hpp
@@ -2,6 +2,9 @@
 
 #include "SpatialBase.h"
 
+#include <vector>
+#include <limits>
+
 namespace utec {
 namespace spatial {
 
@@ -11,14 +14,36 @@ namespace spatial {
 template <typename Point>
 class BasicSpatial : public SpatialBase<Point> {
  private:
+  std::vector<Point> v;
 
  public:
   BasicSpatial() {};
-  void insert(const Point& new_point) override {}
+  void insert(const Point& new_point) override
+  {
+    v.push_back(new_point);
+  }
 
   // El punto de referencia no necesariamente es parte del dataset
-  Point nearest_neighbor(const Point& reference) override { return Point({0,0}); }
+  Point nearest_neighbor(const Point& reference) override
+  {
+    Point nearest = reference;
+    double min_distance = std::numeric_limits<double>::infinity();
+
+    for(const auto& p: v)
+    {
+      double distance = reference.distance(p);
+      if(distance < min_distance)
+      {
+        min_distance = distance;
+        nearest = p;
+      }
+    }
+
+    return nearest;
+  }
 };
 
 }  // namespace spatial
 }  // namespace utec
+
+// vim: set et ts=2 sw=2:
